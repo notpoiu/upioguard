afd699a3735c1ec3851e2b5b44709b2bac7172d1
Nathan Shin <mastertofu12345@gmail.com>
2024-07-30 17:10:31 -0400

    fix: debug

--- public/scripts/main.lua
+++ public/scripts/main.lua
@@ -1,15 +1,16 @@
 -- we loadstring this script
 -- _G.ug_key = "your key"
-local BASE_URL = "https://upioguard.vercel.app"
-
 local req = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request 
 
 local response = req({
-  Url = BASE_URL .. "/api/script",
+  Url = "https://upioguard.vercel.app/api/script",
   Method = "GET",
   Headers = {
     ["user-upioguard-key"] = _G.ug_key
   }
 })
+
+print("response:")
+print(response.body)
 
 loadstring(response.body)()
