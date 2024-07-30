c6400cbf1f7e816c833b9b75e88994d1f9647778
Nathan Shin <mastertofu12345@gmail.com>
2024-07-30 09:46:12 -0400

    fix: unable to fetch userid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <main>
+      <h1>Hi idc about frontend rn</h1>
+      {!session && (
+        <form action={async () => {
+          "use server";
+          await signIn("discord");
+        }}>
+          <Button>Login/Sign in via Discord</Button>
+        </form>
+      )}
 
+      {session && (
+        <>
+          <h2>Logged in as {session?.user?.name}</h2>
+          <p>ID: {session?.user?.id}</p>
+          <form action={async () => {
+            "use server";
+            await signOut();
+          }}>
+            <Button>Logout</Button>
+          </form>
+        </>
+      )}
     </main>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -4,13 +4,18 @@ import Discord from "next-auth/providers/discord"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-        token.name = user.name
-        token.email = user.email
+    async jwt({ token, user, profile }) {
+      if (profile) {
+        token.id = profile.id
       }
       return token
+    },
+
+    async session({ session, token }) {
+      // @ts-ignore
+      session.user.id = token.id
+      
+      return session
     },
   },
 })
