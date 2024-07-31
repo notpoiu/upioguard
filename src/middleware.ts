1ae99d340d31a7e5e545d7fd242e79eb2559eecb
Nathan Shin <mastertofu12345@gmail.com>
2024-07-30 23:44:31 -0400

    feat: rewrite the url thing so it looks cooler like loader thing idk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,25 @@
 import { auth } from "@/auth";
+import { url } from "inspector";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import path from "path";
+
+function get_pathname(request: NextApiRequest) {
+  const url_parts = request.url?.split("/");
+  url_parts?.splice(0, 3);
+  let url_path = "/"+url_parts?.join("/");
+
+  return url_path;
+}
 
 export default async function middleware(request: NextApiRequest, response: NextApiResponse) {
-  auth(request, response);
+  await auth(request, response);
 
+  const pathname = get_pathname(request);
 
+  if (pathname.startsWith("/scripts/loaders/")) {
+    const script_id = pathname.split("/")[pathname.split("/").length - 1].replace(".lua", "").replace(".luau", "");
+
+    return NextResponse.rewrite(new URL(`/api/script/initial/${script_id}`, request.url));
+  }
 }
