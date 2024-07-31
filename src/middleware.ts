3180eb7d9ade99dba2a10d0492fc3bba326f4d43
Nathan Shin <mastertofu12345@gmail.com>
2024-07-31 00:46:12 -0400

    fix: add error handling for non-existent scripts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import { url } from "inspector";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import path from "path";
+import { db } from "./db";
+import { eq } from "drizzle-orm";
+import { project } from "./db/schema";
+import { notFound } from "next/navigation";
 
 function get_pathname(request: NextApiRequest) {
   const url_parts = request.url?.split("/");
@@ -19,6 +23,12 @@ export default async function middleware(request: NextApiRequest, response: Next
 
   if (pathname.startsWith("/scripts/loaders/")) {
     const script_id = pathname.split("/")[pathname.split("/").length - 1].replace(".lua", "").replace(".luau", "");
+
+    const is_script = await db.select().from(project).where(eq(project.project_id, script_id));
+
+    if (is_script.length < 1) {
+      return NextResponse.rewrite(new URL(`/404`, request.url));
+    }
 
     return NextResponse.rewrite(new URL(`/api/script/initial/${script_id}`, request.url));
   }
