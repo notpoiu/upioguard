bdfaa692c1e16dbf18d8158739fffc778e846684
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-08-03 00:16:19 +0200

    updated init script renamed file extension of init script cuz we are actually not in lua 😱

--- src/app/api/script/[script_id]/execute/initial_redirect_gen/route.ts
+++ src/app/api/script/[script_id]/execute/initial_redirect_gen/route.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, { params}: { params: { script_id: string } }) {
-  const file_path = path.join(process.cwd(), 'scripts', 'redirect_initial.lua');
+  const file_path = path.join(process.cwd(), 'scripts', 'redirect_initial.luau');
   const initial_script = fs.readFileSync(file_path, "utf8");
   
   let script = initial_script.replaceAll("${origin}", request.nextUrl.origin);
