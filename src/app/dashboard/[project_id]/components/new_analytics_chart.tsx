f0724d5db1a9e245cd27da6f5df2403b2d1717a6
Nathan Shin <mastertofu12345@gmail.com>
2024-08-06 15:01:16 -0400

    fix: analytics page showing wrong date

--- src/app/dashboard/[project_id]/components/new_analytics_chart.tsx
+++ src/app/dashboard/[project_id]/components/new_analytics_chart.tsx
@@ -69,7 +69,7 @@ export function AnalyticsChartComponent({project_id}: { project_id: string }) {
 
       for (let i = 0; i < 7; i++) {
         const date = new Date();
-        date.setDate(date.getDate() - date.getDay() + i);
+        date.setDate(date.getDate() - date.getDay() + i + 1);
         organized_data_test[normalizeToUTC(date)] = {
           desktop: 0,
           mobile: 0,
