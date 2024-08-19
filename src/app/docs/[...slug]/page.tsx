f3c1d9a9fb90078bfbee17bedd9a82441b599d9d
Nathan Shin <mastertofu12345@gmail.com>
2024-08-19 09:05:10 -0400

    fix: add 404 handling

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,4 +1,10 @@
+import { notFound } from "next/navigation"
+
 export default async function Docs({ params }: { params: { slug: string[] } }) {
-  const component = await import(`@/docs/${params.slug.join("/")}.mdx`)
-  return <component.default />
+  try{
+    const component = await import(`@/docs/${params.slug.join("/")}.mdx`)
+    return <component.default />
+  } catch (e) {
+    return notFound()
+  }
 }
