7fa7c38dbe21a34fd53bb2fc6f2d96386da127bd
Nathan Shin <mastertofu12345@gmail.com>
2024-08-19 08:38:28 -0400

    refactor: remove unused imports

--- src/app/api/script/[script_id]/manage/blacklist/remove/route.ts
+++ src/app/api/script/[script_id]/manage/blacklist/remove/route.ts
@@ -1,11 +1,8 @@
-import { auth } from "@/auth";
 import { db } from "@/db";
-import { project_api_keys } from "@/db/schema";
 import { NextRequest } from "next/server";
 import { eq } from "drizzle-orm";
 import { banned_users } from "@/db/schema";
 import { NextResponse } from "next/server";
-import { validate_permissions } from "@/app/dashboard/server";
 
 export async function POST(req: NextRequest) {
   let { hwid } = await req.json();
