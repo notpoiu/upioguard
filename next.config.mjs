fa282213c12763f2c3dbb01cbdd070af2b78dd95
Nathan Shin <mastertofu12345@gmail.com>
2024-08-19 09:20:13 -0400

    remove docs testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import createMDX from '@next/mdx'
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-};
+const nextConfig = {};
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
-
-export default withMDX(nextConfig);
+export default nextConfig;
